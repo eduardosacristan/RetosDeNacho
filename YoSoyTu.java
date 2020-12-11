27b41be9d31420a44265eb123cc1e92b6becd2aa
eduardosacristan <73886009+eduardosacristan@users.noreply.github.com>
2020-12-11 22:33:56 +0100

    Add files via upload

---
+++ YoSoyTu.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class YoSoyTu
+{
+	public static void main(String[]args)
+    {
+        Scanner sc = new Scanner (System.in);
+
+        int inputs; 
+        int meter = 0;
+        inputs = sc.nextInt();
+        sc.nextLine();
+       
+        do
+        {
+            String name = sc.nextLine();
+            String relationship = sc.nextLine();
+
+            if (name.equals("Luke") && relationship.equals("padre"))
+                System.out.println("TOP SECRET");
+            else 
+            {
+                System.out.printf("%s, yo soy tu %s", name, relationship);
+                System.out.println();
+            }
+            meter++;
+        }
+        while(meter < inputs);
+    }
+}

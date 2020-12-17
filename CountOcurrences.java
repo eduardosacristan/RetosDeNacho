eb3f9f240f05b5599f4b308007bc9db303ef8cef
eduardosacristan <73886009+eduardosacristan@users.noreply.github.com>
2020-12-17 20:20:05 +0100

    esto no va...

---
+++ CountOcurrences.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CountOcurrences {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter first phrase");
+        String phraseA = sc.nextLine();
+
+        System.out.println("Enter second phrase");
+        String phraseB = sc.nextLine();
+
+        System.out.println("Enter number of ocurrences");
+        int ocurrences = sc.nextInt();
+        sc.close();
+        
+        if (countString(phraseA, phraseB, ocurrences) == true)
+            System.out.println("Yes, it is");
+        else    
+            System.out.println("No, it is not");
+    }
+    public static boolean countString(String phraseA, String phraseB, int ocurrences) {
+        boolean yesVeryWell = false;
+        int positionsMeter = 0;
+
+        while (phraseA.indexOf(phraseB) >= 0) {
+            positionsMeter++;
+            phraseA.replaceFirst(phraseB, "");
+        }
+
+        if (positionsMeter == ocurrences)
+            yesVeryWell = true;
+
+        return yesVeryWell;
+    }
+}
